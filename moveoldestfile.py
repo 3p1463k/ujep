31430224b3aec63ae711af2cd9ed407a6be549dd
3p1463k <3p1463k@users.noreply.github.com>
2019-01-02 20:14:04 +0100

    Update moveoldestfile.py

--- moveoldestfile.py
+++ moveoldestfile.py
@@ -1,17 +1,26 @@
+#!/usr/bin/env python
 import os
 import shutil
 import pyperclip
 
 def move_oldest_file():
-    path = 'U:\\'
+    
+    path = '/path/to/file/'
 
     os.chdir(path)
+    
     files = sorted(os.listdir(os.getcwd()), key=os.path.getmtime)
+    
     oldest = files[0]
+    
     newest = files[-1]
-    pyperclip.copy(oldest[11:26])
+    
+    pyperclip.copy(oldest[11:26])#cut out piece of file and copy to mouse
 
     for oldest in files:
+        
         shutil.move(oldest, "  ")
+        
         break
+        
 move_oldest_file()
